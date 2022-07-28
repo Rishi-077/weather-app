b1010e5c4c94d47a62936792baedca30bbd9b1f3
Rishi-077 <rishikesan070@gmail.com>
2022-07-28 21:28:11 +0530

    Desing in input field

--- src/components/SearchWeather.jsx
+++ src/components/SearchWeather.jsx
@@ -1,22 +1,34 @@
 import axios from "axios";
 import React, {useState, useEffect} from "react";
-
+import '../App.css'
 function SearchWeather ()
 {
-  const [ search, setSearch ] = useState( "london" );
+  const [ search, setSearch ] = useState( 'london' );
   const [ data, setData ] = useState( [] );
-  const [ input, setInput ] = useState( "" );
+  const [ input, setInput ] = useState( ' ' );
   
   let componentMounted = true;
 
   useEffect( () =>
   {
+    // const fetchWeather = async () =>
+    // {
+    //   const response = await fetch( `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=0aa8229c250d5c934ea4436242c15f0f` );
+    //   if ( componentMounted )
+    //   {
+    //     setData( await response.json() );
+    //   }
+    //   return () => 
+    //   {
+    //     componentMounted = false;
+    //   };
+    // }
     fetchWeather();
-  }, [] );
+  }, [search]);
   
   const fetchWeather = async () =>
   {
-    await axios.get( `https://api.openweathermap.org/data/2.5/weather?q=${ search }&appid=0aa8229c250d5c934ea4436242c15f0f` )
+    await axios.get( `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=0aa8229c250d5c934ea4436242c15f0f` )
       .then( response =>
       {
         if ( componentMounted )
@@ -65,14 +77,30 @@ function SearchWeather ()
   
   //Date
 
-  let 
+  let d = new Date();
+  let date = d.getDate();
+  let year = d.getFullYear();
+  let month = d.toLocaleString( 'default', { month: 'long' } );
+  let day = d.toLocaleString( 'default', { weekday: 'long' } );
 
+  //Time
+
+  let time = d.toLocaleString( [], {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+  } );
+
+  // Fahrenheit to celsius
   let temp = ( data.main.temp - 273.15 ).toFixed( 2 );
   let temp_min = ( data.main.temp_min - 273.15 ).toFixed( 2 );
   let temp_max = ( data.main.temp_max - 273.15 ).toFixed( 2 );
 
-
-
+  const handleSubmit = (event) => 
+  {
+    event.preventDefault();
+    setSearch(input);
+  }
 
   return (
     <div>
@@ -80,22 +108,32 @@ function SearchWeather ()
         <div className="row justify-content-center">
           <div className="col-md-5">
             <div class="card text-white text-center border-0">
-              <img src={`https://source.unsplash.com/600x900/?${data.weather[0].main}`} class="card-img" alt="..." />
+              <img src={`https://source.unsplash.com/600x900/?${data.weather[0].main}`} class="card-img" alt="image" />
               <div class="card-img-overlay">
-                <form>
+                <form onSubmit = {handleSubmit}>
                   <div class="input-group mb-4 w-75 mx-auto">
                     <input type="text"
-                      class="form-control"
-                      placeholder="search city" aria-label="search city" aria-describedby="basic-addon2" />
-                    <span
-                      type='submit'
-                      class="input-group-text" id="basic-addon2"><i class="fa fa-search"></i></span>
+                      // class="form-control"
+                      placeholder="search city" aria-label="search city" aria-describedby="basic-addon2"
+                      name="search"
+                      value={input}
+                      onChange={(e) => setInput( e.target.value )}
+                      required
+                    />
+                    <button
+                       type='submit'
+                    // class="input-group-text"
+                    >
+                      <i class="fa fa-search"></i>
+                    </button>
                   </div>
                 </form>
                 <div className="bg-dark bg-opacity-50 py-3">
                   <h2 class="card-title">{data.name}</h2>
                   <p class="card-text lead">
-                    Thursday, october 12, 2022
+                    {day}, {month} {date}, {year}
+                    <br />
+                    {time} 
                   </p>
                   <hr />
                   <i className={`fa ${ emoji } fa-4x`}></i>
